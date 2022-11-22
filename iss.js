c3ab709202c88138b71a0076e529b680892ad710
egesenli <egesenli@gmail.com>
2022-11-22 14:03:43 -0500

    Refactored iss and index. Added fetchCoordsByIP.

--- iss.js
+++ iss.js
@@ -11,12 +11,11 @@ const request = require('request');
 
 const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
-
   request('https://api.ipify.org?format=json', (error, response, body) => {
 
-    if (error) {
-      callback(error, null);
-    } else if (response.statusCode !== 200) {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
       callback(Error(msg), null);
       return;
@@ -28,4 +27,27 @@ const fetchMyIP = function(callback) {
   });
 }
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    const parsedBody = JSON.parse(body);
+
+    if (!parsedBody.success) {
+      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+      callback(Error(message), null);
+      return;
+    } 
+
+    const { latitude, longitude } = parsedBody;
+
+    callback(null, {latitude, longitude});
+  });
+};
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
